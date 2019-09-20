6956038413560d6a87ff667eef5c020b9ed1aac7
Aaron Simpson <amsimpso@ksu.edu>
2019-09-20 14:30:31 -0500

    Learning about dispatch and interfaces

---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    interface IBlendable
+    {
+        /// <summary>
+        /// Blends the object
+        /// </summary>
+        /// <returns>A string describing the blended object</returns>
+        string Blend();
+    }
+}
